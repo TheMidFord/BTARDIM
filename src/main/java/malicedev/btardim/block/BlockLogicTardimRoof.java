92770d7b1269ef9a85f95beb9db10e1464fe229c
MidFord <Killer_Mid@outlook.com>
2025-12-06 01:09:47 +0100

    Bounding box shenanigans

--- src/main/java/malicedev/btardim/block/BlockLogicTardimRoof.java
+++ src/main/java/malicedev/btardim/block/BlockLogicTardimRoof.java
@@ -3,11 +3,22 @@ package malicedev.btardim.block;
 import net.minecraft.core.block.Block;
 import net.minecraft.core.block.BlockLogic;
 import net.minecraft.core.block.material.Material;
+import net.minecraft.core.util.phys.AABB;
+import net.minecraft.core.world.World;
 import net.minecraft.core.world.WorldSource;
+
+import java.util.ArrayList;
 
 public class BlockLogicTardimRoof extends BlockLogic {
 	public BlockLogicTardimRoof(Block<?> block, Material material) {
 		super(block, material);
+		this.setBlockBounds(0.0F, 0.0F, 0.0F, 1.0F, 0.5F, 1.0F);
+	}
+
+	@Override
+	public void getCollidingBoundingBoxes(World world, int x, int y, int z, AABB aabb, ArrayList<AABB> aabbList) {
+		this.addIntersectingBoundingBox(aabb, AABB.getTemporaryBB(0.0F, 0.0F, 0.0F, 1.0F, 0.5F, 1.0F), aabbList);
+		this.addIntersectingBoundingBox(aabb, AABB.getTemporaryBB(0.0F, 0.0F, 0.0F, 0.5F, 0.5F, 0.5F).move(x, y + 0.5, z), aabbList);
 	}
 
 	@Override
